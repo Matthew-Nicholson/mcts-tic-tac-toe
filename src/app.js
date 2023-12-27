dc3618e7dcc9ff9a658afaaa1bc34e4c49c7819c
Matt Nicholson <matthewn1991@gmail.com>
2023-12-27 15:18:12 -0500

    init web component

---
+++ src/app.js
@@ -0,0 +1,7 @@
+class TicTacToe extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `<p>Tic Tac Toe game will go here</p>`;
+    }
+}
+
+customElements.define('tic-tac-toe', TicTacToe);
