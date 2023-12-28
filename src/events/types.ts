514a8dfc65a8e46a4704ed98ad3f48e965399a01
Matt Nicholson <matthewn1991@gmail.com>
2023-12-27 22:25:31 -0500

    create initial event bus

--- src/events/types.ts
+++ src/events/types.ts
@@ -1,7 +1,14 @@
-export enum Events {
+export enum Event {
   gameStart,
   player1Moved,
   player2Moved,
   aiMoved,
   gameOver,
+}
+
+export interface EventHandler<T> {
+  publish: (event: Event, data: T) => void;
+  subscribe: (event: Event, callback: (data: T) => void) => void;
+  unsubscribe: (event: Event, callback: (data: T) => void) => void;
+  subscribeOnce: (event: Event, callback: (data: T) => void) => void;
 }
