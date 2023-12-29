7ba6faeaf587f04250a2031dcf6c4bc26b527049
Matt Nicholson <matthewn1991@gmail.com>
2023-12-29 01:02:25 -0500

    ticTacToeGame

--- src/shared/types/pieces.ts
+++ src/shared/types/pieces.ts
@@ -1,4 +1,6 @@
-export const Pieces = {
-  x: "X",
-  o: "O",
-} as const;
+export enum Pieces {
+  x = "X",
+  o = "O",
+}
+// Normally we would use a const here, because the value actually matters.
+// Here, we need 'pieces' available as a type, so the enum is fine.
