e4a2752df4762f56fc80e8edee39e08bd75c9e8b
AbrahamDN <abe.ndinga@gmail.com>
2024-01-04 11:07:53 +0000

    add game animations & winner conditions

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,34 @@
 import { TicTacToeGame } from "./game/ticTacToeGame";
 import { Square } from "./game/types";
+import { isWin } from "./game/utils/isWin";
+import { Pieces } from "./shared/types/pieces";
 import { PlayerTypes } from "./shared/types/playerTypes";
 import "./web/components/gameBoard";
 import "./web/components/scoreBoard";
 import { cells, getCellValues } from "./web/utils/getCellValues";
 
-const game = new TicTacToeGame(PlayerTypes.human, PlayerTypes.ai);
-
 class TicTacToe extends HTMLElement {
+  private game: TicTacToeGame;
+
+  constructor() {
+    super();
+    this.game = {} as TicTacToeGame;
+  }
+
   connectedCallback() {
+    this.initializeGame(PlayerTypes.human, PlayerTypes.ai);
+
     this.innerHTML = `
-        <h2 class='to_move'></h2>
+        <h2 class='subtitle'></h2>
         <game-board></game-board>
         <score-board></score-board>
     `;
 
+    const game = this.game;
+    const subtitleEl = document.getElementsByClassName("subtitle")[0];
+
     const handlePlayerToMove = (gameCopy: TicTacToeGame) => {
-      document.getElementsByClassName("to_move")[0].textContent =
-        `${gameCopy.toMove.name} move`;
+      subtitleEl.textContent = `${gameCopy.toMove.name} move`;
     };
     handlePlayerToMove(game);
 
@@ -27,6 +38,10 @@ class TicTacToe extends HTMLElement {
 
       squares.forEach((square) => {
         square.addEventListener("click", () => {
+          // Game won = true
+          if (isWin(game.boardState, game.result as Pieces)) return;
+
+          // Game won = false
           const cellValues: Square = getCellValues(
             square.className.split(" "),
             cells
@@ -36,14 +51,32 @@ class TicTacToe extends HTMLElement {
             square.innerHTML = `<div class='${game.toMove.piece.toLocaleLowerCase()}'></div>`;
 
             game.makeMove(cellValues);
-            game.setResult(game.toMove.piece);
+            game.setResult(
+              game.toMove.type === game.playerX.type
+                ? game.playerO.piece
+                : game.playerX.piece
+            );
 
             handlePlayerToMove(game);
+          }
+
+          if (isWin(game.boardState, game.result as Pieces)) {
+            subtitleEl.textContent = game.result + " Wins!";
+            document.querySelector(".board")?.classList.add("win");
+            document
+              .querySelectorAll(
+                `.board.win .square > .${game.result?.toLowerCase()}`
+              )
+              .forEach((el) => el.classList.add("win"));
           }
         });
       });
     };
     handleSquareClick();
+  }
+
+  private initializeGame(player1: PlayerTypes, player2: PlayerTypes) {
+    this.game = new TicTacToeGame(player1, player2);
   }
 }
 
