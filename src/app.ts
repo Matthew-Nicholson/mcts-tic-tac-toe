45b1fc1a4d6f4d5d19c5ef15c5d0fb8c5d6f081a
AbrahamDN <abe.ndinga@gmail.com>
2024-01-04 06:24:35 +0000

    wip: make a helper function to get game square value (getCellValues)

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,10 @@
+import { TicTacToeGame } from "./game/ticTacToeGame";
+import { Square } from "./game/types";
+import { PlayerTypes } from "./shared/types/playerTypes";
 import "./web/components/gameBoard";
+import { cells, getCellValues } from "./web/utils/getCellValues";
+
+const game = new TicTacToeGame(PlayerTypes.human, PlayerTypes.ai);
 
 class TicTacToe extends HTMLElement {
   connectedCallback() {
@@ -7,6 +13,24 @@ class TicTacToe extends HTMLElement {
          <game-board></game-board>
       </section>
     `;
+
+    // Function to add click event to squares
+    const handleSquareClick = () => {
+      const squares = this.querySelectorAll(".square");
+
+      squares.forEach((square) => {
+        square.addEventListener("click", () => {
+          const cellValues: Square = getCellValues(
+            square.className.split(" "),
+            cells
+          );
+
+          // game.makeMove(cellValues);
+          console.info(cellValues);
+        });
+      });
+    };
+    handleSquareClick();
   }
 }
 
