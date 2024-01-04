7dd0c2eec1fbd9d4b0f456986951cafe501290d6
AbrahamDN <abe.ndinga@gmail.com>
2024-01-04 08:55:32 +0000

    add X & O on click + initial score component

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { TicTacToeGame } from "./game/ticTacToeGame";
 import { Square } from "./game/types";
 import { PlayerTypes } from "./shared/types/playerTypes";
 import "./web/components/gameBoard";
+import "./web/components/scoreBoard";
 import { cells, getCellValues } from "./web/utils/getCellValues";
 
 const game = new TicTacToeGame(PlayerTypes.human, PlayerTypes.ai);
@@ -9,10 +10,16 @@ const game = new TicTacToeGame(PlayerTypes.human, PlayerTypes.ai);
 class TicTacToe extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <section class='game'>
-         <game-board></game-board>
-      </section>
+        <h2 class='to_move'></h2>
+        <game-board></game-board>
+        <score-board></score-board>
     `;
+
+    const handlePlayerToMove = (gameCopy: TicTacToeGame) => {
+      document.getElementsByClassName("to_move")[0].textContent =
+        `${gameCopy.toMove.name} move`;
+    };
+    handlePlayerToMove(game);
 
     // Function to add click event to squares
     const handleSquareClick = () => {
@@ -25,8 +32,14 @@ class TicTacToe extends HTMLElement {
             cells
           );
 
-          // game.makeMove(cellValues);
-          console.info(cellValues);
+          if (game.isLegalMove(cellValues, game.boardState)) {
+            square.innerHTML = `<div class='${game.toMove.piece.toLocaleLowerCase()}'></div>`;
+
+            game.makeMove(cellValues);
+            game.setResult(game.toMove.piece);
+
+            handlePlayerToMove(game);
+          }
         });
       });
     };
